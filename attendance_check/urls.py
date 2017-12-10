44d225035eb9f7c746cc18654507401aaf910d2c
= <=>
2017-12-10 21:28:01 +0900

    uuidCheker.py 생성 시간 값을 불러올 시 시차문제 해결 강의시작 날짜 출력 빠른강좌찾기 URL접근시 자신의 강좌 중 활성화 된 강좌를 응답 강의리스트 출력 시 출석 대기중인(기록카드가 만들어지지 않은학생)은 결석으로 뜨도록 설정 결석 플래그 변경기능 추가 비콘 시차감지 알고리즘 추가 비콘 출석자 플래그 생성

--- attendance_check/urls.py
+++ attendance_check/urls.py
@@ -42,7 +42,8 @@ from .views import (
                     LectureStuCheck,
                     LectureListSearch,
                     LectureCheckedSearchView,
-                    LectureFastestView)
+                    LectureFastestView,
+                    LectureBeaconCheck)
 
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token
@@ -83,6 +84,7 @@ urlpatterns = [
     url(r'^api/lecture/apply/check/status/$', LectureStuCheck.as_view()),
     url(r'^api/lecture/list/search/$', LectureListSearch.as_view()),
     url(r'^api/lecture/list/checked/view/$', LectureCheckedSearchView.as_view()),
+    url(r'^api/lecture/check/uuid/$', LectureBeaconCheck.as_view()),
     url(r'^api/lecture/fastest/view/$', LectureFastestView.as_view()),
     # url(r'^api/lecture/stop/$', LectureStopView.as_view()),
     # url(r'^api/lecture/check/$', LectureStopView.as_view()),
